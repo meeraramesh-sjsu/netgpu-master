3c52890e57f7ddb354878352921235d5af6a6de9
meeraramesh-sjsu <meekar67@gmail.com>
2016-08-03 10:45:29 -0700

    Minor Changes!

--- src/Analysis/Libs/Gpu/Macros/General.h
+++ src/Analysis/Libs/Gpu/Macros/General.h
@@ -63,7 +63,6 @@ The NetGPU framework is distributed in the hope that it will be useful, but WITH
 #define GET_HEADER_TCP_POINTER(level)\
 	PACKET->headers.offset[level]
 
-
 //Gets field safely, to get disaligned fields 
 #define GET_FIELD(field) cudaNetworkToHost(cudaSafeGet(&(field))) //TODO: ENDIANISME ELIMINAR EL CUDANETWORKTOHOST
 
