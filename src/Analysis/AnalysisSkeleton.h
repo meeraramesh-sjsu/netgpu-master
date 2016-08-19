3ed36c0a37ff327325159dd4aa291dbf436b2328
meeraramesh-sjsu <meekar67@gmail.com>
2016-08-19 08:11:49 -0700

    Minor Changes!

--- src/Analysis/AnalysisSkeleton.h
+++ src/Analysis/AnalysisSkeleton.h
@@ -782,16 +782,16 @@ void COMPOUND_NAME(ANALYSIS_NAME,launchAnalysis_wrapper)(PacketBuffer* packetBuf
 		//Launch hook (or preHook if window is set)
 		COMPOUND_NAME(ANALYSIS_NAME,hooks)(packetBuffer, results, state,auxBlocks);
 
-
-		//Frees results
-		cudaAssert(cudaFreeHost(results));
-		//free(results);
 		// cudaDeviceReset causes the driver to clean up all state. While
 		// not mandatory in normal operation, it is good practice.  It is also
 		// needed to ensure correct operation when the application is being
 		// profiled. Calling cudaDeviceReset causes all profile data to be
 		// flushed before the application exits
-		cudaDeviceReset();
+		cudaAssert(cudaDeviceReset());
+		//Frees results
+		cudaAssert(cudaFreeHost(results));
+		//free(results);
+
 	}
 }
 
