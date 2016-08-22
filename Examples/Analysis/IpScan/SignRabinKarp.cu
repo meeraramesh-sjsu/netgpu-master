32cb321b446a6f1ea97a8c4b31aa55caf0d4a9e9
meeraramesh-sjsu <meekar67@gmail.com>
2016-08-22 14:12:08 -0700

    Minor Changes!

--- Examples/Analysis/IpScan/SignRabinKarp.cu
+++ Examples/Analysis/IpScan/SignRabinKarp.cu
@@ -20,7 +20,7 @@ __device__ int memCmpDev(char *input, char *pattern, int offset,int N,int M)
 	}
 }
 
-__global__ void findIfExistsCu(char input[], int  N, char pattern[], int M,int RM,int inputHash,int patHash,int result)
+__global__ void findIfExistsCu(char input[], int  N, char pattern[], int M,int RM,int inputHash,int patHash,int& result)
 {
 	int x = threadIdx.x + blockIdx.x * blockDim.x;
 	if (threadIdx.x==0 && inputHash == patHash && memCmpDev(input, pattern, 0, N, M) == 0) result = 0;
