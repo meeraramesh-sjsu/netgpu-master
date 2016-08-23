392fa85927b3245a79f815e2d39f7ca29e19f725
meeraramesh-sjsu <meekar67@gmail.com>
2016-08-22 21:04:15 -0700

    Minor Changes!

--- Examples/Analysis/IpScan/SignRabinKarp.cu
+++ Examples/Analysis/IpScan/SignRabinKarp.cu
@@ -16,9 +16,6 @@ using namespace std;
 
 __device__ int memCmpDev(char *input, char *pattern, int offset,int N,int M)
 {
-	if (threadIdx.x == offset)
-	{
-		if (N - offset < M) return -1;
 		bool result = true;
 		int j = 0;
 		for (int i = offset; i < offset + M && result; i++)
@@ -26,27 +23,18 @@ __device__ int memCmpDev(char *input, char *pattern, int offset,int N,int M)
 			if (input[i] != pattern[j++]) result = false;
 		}
 		return !result;
-	}
 }
 
-__global__ void findIfExistsCu(char* input, int  N, char* pattern, int M,int RM,int inputHash,int patHash,int* result)
+__global__ void findIfExistsCu(char* input, int  N, char* pattern, int M,int patHash,bool* result)
 {
 	int x = threadIdx.x + blockIdx.x * blockDim.x;
-	if (inputHash == patHash && memCmpDev(input, pattern, 0, N, M) == 0) 
-		{int x = 0; result = &x;}
-	else
+	if(x<=N-M)
 	{
-		if (x >= M && N - M >= M)
-		{
-			inputHash = (inputHash + 997 - (input[x - M] * RM) % 997) % 997;
-			inputHash = (inputHash * 256 + input[x]) % 997;
-			printf("InputHash = %d, patHash = %d \n", inputHash,patHash);
-			if (inputHash == patHash && memCmpDev(input, pattern, x - M + 1, N, M) == 0)
-				{
-				int y = x-1;
-				printf("y = %d \n",y);
-				result= &y;}
-		}
+		int hy,i;
+		for(int hy=i=0;i<M;i++)
+			hy = (hy * 256 + input[i+x]) % 997;
+		if(hy == patHash && memCmpDev(input,pattern,x,N,M) == 0)
+			result[x]=1;
 	}
 }
 
@@ -56,34 +44,30 @@ int main()
 	char pattern[] = "AB";
 	int M = 2;
 	int patHash = 0;
-	int initInputHash = 0;
 	int N = 6;
 	char* d_input;
 	char* d_pattern;
 	int* d_result;
-	int result = -1;
-	int RM = 1;
-	for (int i = 1; i <= M - 1; i++)
-		RM = (256 * RM) % 997;
-	if (N >= M)
+	int* result;
+	for (int i = 0; i < M; i++)
 	{
-		for (int i = 0; i < M; i++)
-		{
-			initInputHash = (initInputHash * 256 + input[i]) % 997;
-			patHash = (patHash * 256 + pattern[i]) % 997;
-		}		
-		cout<<"initially Input hash = "<<initInputHash<<"pattern hash = "<<patHash<<endl;
-		cudaAssert(cudaMalloc((void **)&d_input, N * sizeof(char)));
-		cudaAssert(cudaMalloc((void **)&d_pattern, M * sizeof(char)));
-		cudaAssert(cudaMalloc((void **)&d_result, sizeof(int)));
-		cudaAssert(cudaMemcpy(d_input, input, N * sizeof(char), cudaMemcpyHostToDevice));
-		cudaAssert(cudaMemcpy(d_pattern, pattern, M * sizeof(char), cudaMemcpyHostToDevice));
-		dim3 block(N, 0, 0);
-		dim3 grid(1, 0, 0);
-		findIfExistsCu <<<1, 6>>> (d_input,N,d_pattern,M,RM,initInputHash,patHash,d_result);
-		cudaAssert(cudaThreadSynchronize());
-		cudaMemcpy(&result, d_result, sizeof(int), cudaMemcpyDeviceToHost);
-	}
-	cout << "Result = "<<result;
-	return 0;
+		patHash = (patHash * 256 + pattern[i]) % 997;
+	}		
+	malloc((void **)&result,(N-M)*sizeof(int));
+	cout<<"initially Input hash = "<<initInputHash<<"pattern hash = "<<patHash<<endl;
+	cudaAssert(cudaMalloc((void **)&d_input, N * sizeof(char)));
+	cudaAssert(cudaMalloc((void **)&d_pattern, M * sizeof(char)));
+	cudaAssert(cudaMalloc((void **)&d_result,(N-M)*sizeof(int)));
+	cudaAssert(cudaMemcpy(d_input, input, N * sizeof(char), cudaMemcpyHostToDevice));
+	cudaAssert(cudaMemcpy(d_pattern, pattern, M * sizeof(char), cudaMemcpyHostToDevice));
+	dim3 block(N, 0, 0);
+	dim3 grid(1, 0, 0);
+	findIfExistsCu <<<1, 6>>> (d_input,N,d_pattern,M,patHash,d_result);
+	cudaAssert(cudaThreadSynchronize());
+	cudaMemcpy(&result, d_result, sizeof(int), cudaMemcpyDeviceToHost);
+}
+
+for(int i=0;i<=N-M;i++)
+	cout << result[i]<<" ";
+return 0;
 }
