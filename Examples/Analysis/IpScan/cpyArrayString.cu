b011543a8546e41c135ee537337acbf5e9b9367a
meeraramesh-sjsu <meekar67@gmail.com>
2016-09-29 18:03:55 -0700

    Copying an array of strings to the GPU, by flattening the 2D array to 1D array. Considering the array of strings to be represented by a vector of strings, so it is easy to indentify the size of individual pattern and total no of patterns.

--- Examples/Analysis/IpScan/cpyArrayString.cu
+++ Examples/Analysis/IpScan/cpyArrayString.cu
@@ -2,14 +2,13 @@
 #include <iostream>
 
 using namespace std;
-//#define max_text_length 12;
+
 __device__ int memCmpDev(char *input, char *pattern,int *indexes,int i,int offset,int M)
 {
 	bool result = true;
 	int j = indexes[2*i];
 	for (int i = offset; i < offset + M && result; i++)
 	{
-//	printf("MemCmpDev , input = %d, pattern = %d", input[i],pattern[j]);
 		if (input[i] != pattern[j++]) result = false;
 	}
 	return !result;
@@ -29,13 +28,10 @@ __global__ void findIfExistsCu(char* d_input,int inputLen,char* pattern,int * in
    	for(hy=j=0;j<M;j++)
 	hy = (hy * 256 + d_input[j+x]) % 997;
 	
-	//printf("x = %d  %d", x , patHash[i]);
-	//printf("threadIdx = %d, hy = %d ,i = %d\n",x,hy,i);
 	if(hy == patHash[i] && memCmpDev(d_input,pattern,indexes,i,x,M) == 0)
 	d_result[i]=1;
 	}
 	}
-	//runtime[x] = int(stopTime - startTime);
 }
 
 void calcPatHash(char *tmp[], int *patHash, int numStr)
@@ -48,45 +44,57 @@ void calcPatHash(char *tmp[], int *patHash, int numStr)
  patHash[i] = (patHash[i] * 256 + tmp[i][index]) % 997;
  index++;
  }
-//cout<<"PatHash = "<<patHash[i]<<"Of index = "<<i<<endl;
  }
 }
 
 int main(){
 
-  char input[] = "absome textcd";
-  int inputLen = 13;
-  char *d_input;
- int num_str = 3;
- char *tmp[num_str];
+string input = "absome textcd";
+int inputLen = input.size();
+char *d_input;
+ 
+ //char *tmp[num_str];
+ vector<string> tmp;
+ tmp.push_back("some text");
+ tmp.push_back("ab");
+ tmp.push_back("text");
+ 
  int *patHash;
 int *d_patHash;
  int* d_result;
  int* result;
- int max_text_length = 12;
+ //int max_text_length = 12;
+int num_str = tmp.size();
 
  patHash = (int*) malloc(num_str * sizeof(int));
- tmp[0] = (char*) malloc(max_text_length*sizeof(char));
+ /*tmp[0] = (char*) malloc(max_text_length*sizeof(char));
  tmp[1] = (char*) malloc(max_text_length*sizeof(char));
- tmp[2] = (char*) malloc(max_text_length*sizeof(char));
+ tmp[2] = (char*) malloc(max_text_length*sizeof(char));*/
+ /*tmp[0] = "some text";
+ tmp[1] = "ab";
+ tmp[2] = "text";*/
+ 
  result = (int*) malloc((num_str)*sizeof(int));
  
  memset(result,0,num_str*sizeof(int));
- tmp[0] = "some text";
- tmp[1] = "ab";
- tmp[2] = "text";
 
  int stridx[2*num_str];
+ memset(stridx,0,2*num_str);
  int *d_stridx;
- stridx[0] = 0;
+ for(int i=0,j=0;i<2*num_str;i+=2)
+ {
+ stridx[i]= stridx[i+1];
+ stridx[i+1]= stridx[i]+tmp[j++].size();
+ }
+ /*stridx[0] = 0;
  stridx[1] = 9;
  stridx[2] = 9;
  stridx[3] = 11;
  stridx[4] = 11;
- stridx[5] = 15;
+ stridx[5] = 15;*/
  
  char *a, *d_a;
- a = (char *)malloc(num_str*max_text_length*sizeof(char));
+ a = (char *)malloc(stridx[2*num_str - 1]*sizeof(char));
  //flatten
  int subidx = 0;
  for(int i=0;i<num_str;i++)
@@ -97,13 +105,13 @@ int *d_patHash;
  }
  
 calcPatHash(tmp,patHash,num_str);
-cudaMalloc((void**)&d_a,num_str*max_text_length*sizeof(char));
-cudaMemcpy(d_a, a, num_str*max_text_length*sizeof(char),cudaMemcpyHostToDevice);
+cudaMalloc((void**)&d_a,stridx[2*num_str - 1]*sizeof(char));
+cudaMemcpy(d_a, a, stridx[2*num_str - 1]*sizeof(char),cudaMemcpyHostToDevice);
 cudaMalloc((void**)&d_stridx,num_str*2*sizeof(int));
 cudaMemcpy(d_stridx, stridx,2*num_str*sizeof(int),cudaMemcpyHostToDevice);
 cudaMalloc((void **)&d_input,inputLen * sizeof(char));
 cudaMalloc((void **)&d_result,num_str*sizeof(int));
-cudaMemcpy(d_input, input, inputLen * sizeof(char), cudaMemcpyHostToDevice);
+cudaMemcpy(d_input, input.c_str(), inputLen * sizeof(char), cudaMemcpyHostToDevice);
 cudaMalloc((void **)&d_patHash, num_str * sizeof(int));
 cudaMemcpy(d_patHash,patHash,num_str * sizeof(int), cudaMemcpyHostToDevice);
 cudaMemset(d_result, 0 , num_str * sizeof(int));
