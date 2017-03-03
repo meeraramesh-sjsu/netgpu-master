624175fc03836b18a5685a59a69f354bc26b932d
meeraramesh-sjsu <meekar67@gmail.com>
2017-03-02 18:43:25 -0800

    MC!

--- src/Common/Dissector.cpp
+++ src/Common/Dissector.cpp
@@ -229,7 +229,7 @@ void Dissector::searchWords(vector<string> arr, int k, string text)
 			int patIndex = out[currentState][j];
 			//The size of the output is fixed to the pattern size, but the current State value
 			//can exceed the pattern size
-			if(patIndex>=k) continue;
+			if(patIndex>=k || patIndex<0) continue;
 			    DEBUG2("In searchWords outIndex=%d currentState=%d patIndex=%d",j,currentState,out[currentState][j]);
 				long start = (long) i - arr[patIndex].size() + 1;
 				if(start >= text.size()) continue;
